9cbe6ea291ad0335d85441b848ae6fae3f95bf55
Paul Penketh <paul.penketh@gmail.com>
2022-11-20 10:54:55 +0000

    Initial Commit

---
+++ SumThing/SumThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SumThingApp.swift
+//  SumThing
+//
+//  Created by PAUL PENKETH on 20/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SumThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
